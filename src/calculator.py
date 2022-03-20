54e97031e007eab9fcbd78f1cbcaab19eb943c14
Andrea Benevenuta <andreabenevenuta@Andreas-MacBook-Pro.local>
2022-03-20 15:33:02 +0100

    black and isort

--- src/calculator.py
+++ src/calculator.py
@@ -1,6 +1,8 @@
-import sys, ast
+import ast
+import sys
 
 list_of_argument = sys.argv
+
 
 def sum(num1, num2):
     return int(num1) + int(num2)
@@ -8,7 +10,7 @@ def sum(num1, num2):
 
 def substract(num1, num2):
     return int(num1) - int(num2)
-    
+
 
 if __name__ == "__main__":
     if list_of_argument[1] == "-sum":
