b46e42a96660417ad8779ab7cea3c565f5b36f64
Andrea Benevenuta <andreabenevenuta@Andreas-MacBook-Pro.local>
2022-03-20 15:28:00 +0100

    new_try

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -2,7 +2,7 @@ import subprocess
 
 
 def test_sum() -> None:
-    cmd = ["poetry", "run", "python", "../src/calculator.py", "-sum", "3", "1"]
+    cmd = ["poetry", "run", "python", "src/calculator.py", "-sum", "3", "1"]
     process = subprocess.Popen(
         cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE
     )
@@ -11,7 +11,7 @@ def test_sum() -> None:
 
 
 def test_substract() -> None:
-    cmd = ["poetry", "run", "python", "../src/calculator.py", "-substract", "3", "1"]
+    cmd = ["poetry", "run", "python", "src/calculator.py", "-substract", "3", "1"]
     process = subprocess.Popen(
         cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE
     )
