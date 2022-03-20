a9051a0f6d05580fc15cb0547c55016e4ade2323
Andrea Benevenuta <andreabenevenuta@Andreas-MacBook-Pro.local>
2022-03-20 15:11:17 +0100

    new calculator and tests

---
+++ tests/test_calculator.py
@@ -0,0 +1,19 @@
+import subprocess
+
+
+def test_sum() -> None:
+    cmd = ["poetry", "run", "python", "../src/main.py", "-sum", "3", "1"]
+    process = subprocess.Popen(
+        cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+    result, err = process.communicate()
+    assert float(result.strip()) == 4
+
+
+def test_substract() -> None:
+    cmd = ["poetry", "run", "python", "../src/main.py", "-substract", "3", "1"]
+    process = subprocess.Popen(
+        cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+    result, err = process.communicate()
+    assert float(result.strip()) == 2
