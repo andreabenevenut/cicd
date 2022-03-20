5c860657d609a553ccaf5dba9db33e601620c5ae
Andrea Benevenuta <andreabenevenuta@Andreas-MacBook-Pro.local>
2022-03-20 11:37:00 +0100

    terraform changed

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ terraform {
     subscription_id      = "7a10c5e9-76b4-417b-9f21-ccad0bc5596f"
     resource_group_name  = "rg-rootsacad2022-workshop"
     storage_account_name = "sarootsacad2022workshop"
-    container_name       = "statecontainerbrt"
-    key                  = "azure-rootsacademy.tfstate"
+    container_name       = "state-container-and"
+    key                  = "my-backend-state.tfstate"
   }
 }
 
@@ -18,8 +18,8 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_storage_container" "brt" {
-  name                  = "brt"
+resource "azurerm_storage_container" "containerand" {
+  name                  = "containerand"
   storage_account_name  = "sarootsacad2022workshop"
   container_access_type = "private"
 }
