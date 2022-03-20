351738c0ca66851ea177a47d9f9e4994f73020c1
Andrea Benevenuta <andreabenevenuta@Andreas-MacBook-Pro.local>
2022-03-20 11:31:59 +0100

    changed_calculator

--- src/main.py
+++ src/main.py
@@ -1,17 +1,19 @@
-import argparse
+import sys, ast
 
-parser = argparse.ArgumentParser(description="Process some integers.")
-parser.add_argument(
-    "integers", metavar="N", type=int, nargs="+", help="an integer for the accumulator"
-)
-parser.add_argument(
-    "--sum",
-    dest="accumulate",
-    action="store_const",
-    const=sum,
-    default=max,
-    help="sum the integers (default: find the max)",
-)
+list_of_argument = sys.argv
 
-args = parser.parse_args()
-print(args.accumulate(args.integers))
+def substract(l):
+    return l[0] - l[1]
+    
+def sum(l):
+    return l[0] + l[1]
+
+if __name__ == "__main__":
+    nv = ast.literal_eval(list_of_argument[2])
+    print(list_of_argument)
+    if list_of_argument[1] == "-sum":
+        print(sum(nv))
+    elif list_of_argument[1] == "-substract":
+        print(substract(nv))
+    else:
+        print("Invalid argument!")
